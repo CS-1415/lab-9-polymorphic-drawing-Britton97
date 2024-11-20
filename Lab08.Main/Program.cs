eb5a7efae7f1312aff72645de8b6195543e3bb2b
Britton Whitaker <brittonwhit97@gmail.com>
2024-11-20 06:43:07 -0800

    Project Set Up

---
+++ Lab08.Main/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Lab08;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Circle circle = new Circle();
+        circle.Display();
+    }
+}
